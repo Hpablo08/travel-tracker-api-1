badf3326e3f694153419a9cd9e8dd5cfd19f5831
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-10-27 10:56:17 -0600

    add vercel

--- server.js
+++ server.js
@@ -1,57 +1,57 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3001;
 const app = express();
-const travelers = require('./data/travelers');
-const trips = require('./data/trips');
-const destinations = require('./data/destinations');
+const travelers = require("./data/travelers");
+const trips = require("./data/trips");
+const destinations = require("./data/destinations");
 
 app.locals = {
-  title: 'Travel Tracker API',
+  title: "Travel Tracker API",
   travelers,
   trips,
-  destinations
-}
+  destinations,
+};
 
 app.use(cors());
 app.use(express.json());
 
-const isValidDate = dateString => {
+const isValidDate = (dateString) => {
   var regEx = /^\d{4}[\/](0?[1-9]|1[012])[\/](0?[1-9]|[12][0-9]|3[01])$/;
   return regEx.test(dateString);
-}
+};
 
-app.get('/api/v1/travelers', (req, res) => {
+app.get("/api/v1/travelers", (req, res) => {
   const { travelers } = app.locals;
   res.status(200).json({ travelers });
 });
 
-app.get('/api/v1/travelers/:id', (req, res) => {
+app.get("/api/v1/travelers/:id", (req, res) => {
   const { id } = req.params;
   const { travelers } = app.locals;
 
-  let requestedTraveler = travelers.find(traveler => traveler.id == id);
+  let requestedTraveler = travelers.find((traveler) => traveler.id == id);
 
   if (!requestedTraveler) {
     return res.status(404).json({
-      message: `No traveler found with an id of ${id}`
+      message: `No traveler found with an id of ${id}`,
     });
   }
 
   res.status(200).json(requestedTraveler);
 });
 
-app.get('/api/v1/trips', (req, res) => {
+app.get("/api/v1/trips", (req, res) => {
   const { trips } = app.locals;
   res.status(200).json({ trips });
 });
 
-app.get('/api/v1/destinations', (req, res) => {
+app.get("/api/v1/destinations", (req, res) => {
   const { destinations } = app.locals;
   res.status(200).json({ destinations });
 });
 
-app.post('/api/v1/trips', (req, res) => {
+app.post("/api/v1/trips", (req, res) => {
   const {
     id,
     userID,
@@ -60,14 +60,24 @@ app.post('/api/v1/trips', (req, res) => {
     date,
     duration,
     status,
-    suggestedActivities } = req.body;
+    suggestedActivities,
+  } = req.body;
 
-  const requiredProperties = ['id', 'userID', 'destinationID', 'travelers', 'date', 'duration', 'status', 'suggestedActivities']
+  const requiredProperties = [
+    "id",
+    "userID",
+    "destinationID",
+    "travelers",
+    "date",
+    "duration",
+    "status",
+    "suggestedActivities",
+  ];
 
   for (let requiredParameter of requiredProperties) {
     if (req.body[requiredParameter] === undefined) {
       return res.status(422).json({
-        message: `You are missing a required parameter of ${requiredParameter}`
+        message: `You are missing a required parameter of ${requiredParameter}`,
       });
     }
   }
@@ -75,24 +85,39 @@ app.post('/api/v1/trips', (req, res) => {
   // Check for valid date
   if (!isValidDate(date)) {
     return res.status(422).json({
-      message: `Invalid date format submitted`
-    })
+      message: `Invalid date format submitted`,
+    });
   }
 
   // Check if id already exists
-  const existingTrip = app.locals.trips.find(trip => trip.id === id);
+  const existingTrip = app.locals.trips.find((trip) => trip.id === id);
 
   if (existingTrip) {
-    return res.status(422).json({ message: `Trip with an id of ${id} already exists.` });
+    return res
+      .status(422)
+      .json({ message: `Trip with an id of ${id} already exists.` });
   }
 
-  const existingDestination = app.locals.destinations.find(destination => destination.id === destinationID);
+  const existingDestination = app.locals.destinations.find(
+    (destination) => destination.id === destinationID
+  );
 
   if (!existingDestination) {
-    return res.status(404).json({ message: `Destination with id ${destinationID} doesn't exist.` })
+    return res
+      .status(404)
+      .json({ message: `Destination with id ${destinationID} doesn't exist.` });
   }
 
-  app.locals.trips.push({ id, userID, destinationID, travelers, date, duration, status, suggestedActivities });
+  app.locals.trips.push({
+    id,
+    userID,
+    destinationID,
+    travelers,
+    date,
+    duration,
+    status,
+    suggestedActivities,
+  });
   res.status(201).json({
     message: `Trip with id ${id} successfully posted`,
     newTrip: {
@@ -103,38 +128,56 @@ app.post('/api/v1/trips', (req, res) => {
       date,
       duration,
       status,
-      suggestedActivities
-    }
+      suggestedActivities,
+    },
   });
 });
 
-app.post('/api/v1/destinations', (req, res) => {
+app.post("/api/v1/destinations", (req, res) => {
   const {
     id,
     destination,
     estimatedLodgingCostPerDay,
     estimatedFlightCostPerPerson,
     image,
-    alt
+    alt,
   } = req.body;
 
-  const requiredProperties = ['id', 'destination', 'estimatedLodgingCostPerDay', 'estimatedFlightCostPerPerson', 'image', 'alt'];
+  const requiredProperties = [
+    "id",
+    "destination",
+    "estimatedLodgingCostPerDay",
+    "estimatedFlightCostPerPerson",
+    "image",
+    "alt",
+  ];
 
   for (let requiredParameter of requiredProperties) {
     if (req.body[requiredParameter] === undefined) {
       return res.status(422).json({
-        message: `You are missing a required parameter of ${requiredParameter}`
+        message: `You are missing a required parameter of ${requiredParameter}`,
       });
     }
   }
 
-  const existingDestination = app.locals.destinations.find(destination => destination.id === id);
+  const existingDestination = app.locals.destinations.find(
+    (destination) => destination.id === id
+  );
 
   if (existingDestination) {
-    return res.status(422).json({ message: `Destination with id ${id} already exists.` })
+    return res
+      .status(422)
+      .json({ message: `Destination with id ${id} already exists.` });
   }
 
-  app.locals.destinations.push({ id, destination, estimatedLodgingCostPerDay, estimatedFlightCostPerPerson, image, alt });
+  app.locals.destinations.push({
+    id,
+    destination,
+    estimatedLodgingCostPerDay,
+    estimatedFlightCostPerPerson,
+    image,
+    alt,
+  });
   res.status(201).json({
     message: `Destination with id ${id} successfully posted`,
     newDestination: {
@@ -143,61 +186,74 @@ app.post('/api/v1/destinations', (req, res) => {
       estimatedLodgingCostPerDay,
       estimatedFlightCostPerPerson,
       image,
-      alt
-    }
+      alt,
+    },
   });
 });
 
-app.post('/api/v1/updateTrip', (req, res) => {
+app.post("/api/v1/updateTrip", (req, res) => {
   const { id, status, suggestedActivities } = req.body;
 
   if (!id) {
-    res.status(422).json({ message: 'You are missing a required parameter of id.' });
+    res
+      .status(422)
+      .json({ message: "You are missing a required parameter of id." });
   }
 
-  let foundTrip = app.locals.trips.find(trip => trip.id === id);
+  let foundTrip = app.locals.trips.find((trip) => trip.id === id);
 
   if (!foundTrip) {
-    return res.status(404).json({ message: `No trip with id ${id} found.` })
+    return res.status(404).json({ message: `No trip with id ${id} found.` });
   }
 
-  if (status && ['pending', 'approved'].includes(status)) {
+  if (status && ["pending", "approved"].includes(status)) {
     foundTrip.status = status;
-  } else if (status && !['pending', 'approved'].includes(status)) {
-      return res.status(422).json({ message: 'status must either be pending or approved.' });
+  } else if (status && !["pending", "approved"].includes(status)) {
+    return res
+      .status(422)
+      .json({ message: "status must either be pending or approved." });
   }
 
   if (suggestedActivities && Array.isArray(suggestedActivities)) {
-    foundTrip.suggestedActivities = [...foundTrip.suggestedActivities, ...suggestedActivities];
+    foundTrip.suggestedActivities = [
+      ...foundTrip.suggestedActivities,
+      ...suggestedActivities,
+    ];
   } else if (suggestedActivities && !Array.isArray(suggestedActivities)) {
-    return res.status(422).json({ message: "suggestedActivities must be an array." })
+    return res
+      .status(422)
+      .json({ message: "suggestedActivities must be an array." });
   }
 
   return res.status(200).json({
     message: `Trip #${id} has been modified`,
-    updatedTrip: foundTrip
+    updatedTrip: foundTrip,
   });
 });
 
-app.delete('/api/v1/trips/:id', (req, res) => {
+app.delete("/api/v1/trips/:id", (req, res) => {
   let { id } = req.params;
   const { trips } = app.locals;
 
-  const tripToDelete = trips.find(trip => trip.id === parseInt(id));
+  const tripToDelete = trips.find((trip) => trip.id === parseInt(id));
 
   if (!tripToDelete) {
     return res.status(404).json({
-      message: `No found trip with id of #${id}.`
-    })
+      message: `No found trip with id of #${id}.`,
+    });
   }
 
-  app.locals.trips = trips.filter(trip => trip.id !== parseInt(id));
+  app.locals.trips = trips.filter((trip) => trip.id !== parseInt(id));
 
   res.status(200).json({
-    message: `Trip #${id} has been deleted`
-  })
-})
+    message: `Trip #${id} has been deleted`,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`${app.locals.title} is now running on http://localhost:${port} !`)
+  console.log(
+    `${app.locals.title} is now running on http://localhost:${port} !`
+  );
 });
+
+module.exports = app;
